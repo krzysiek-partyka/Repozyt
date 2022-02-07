3114ae538f9c34ca4fc600f254e7eb79ffb24c54
Krzysiek Partyka <krzyspartyka@gmail.com>
2022-02-07 22:57:01 +0100

    program added

---
+++ Garage/Garage/Bus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    internal class Bus : Vehicle
+    {
+        public Bus (string vehicleTyp, string vehicleColor) : base(vehicleTyp, vehicleColor)
+        {
+            VehicleColor = vehicleColor;
+            VehicleTyp = vehicleTyp;
+        }
+
+        public override void MakeSound()
+        {
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+        }
+        public override void PresentVehicle(Vehicle vehicle)
+        {
+            Console.WriteLine($"Color: {vehicle.VehicleColor} Type: {vehicle.VehicleTyp}");
+            MakeSound();
+        }
+    }
+}
---
+++ Garage/Garage/Vehicle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    abstract class Vehicle
+    {
+        
+        public string VehicleTyp { get; set; }
+        public string VehicleColor { get; set;}
+
+        public Vehicle(string vehicleTyp, string vehicleColor)
+        {
+            VehicleColor = vehicleColor;
+            VehicleTyp = vehicleTyp;
+        }
+        public override string ToString()
+        {
+            return $"{VehicleColor}, {VehicleTyp}";
+        }
+
+        public abstract void MakeSound();
+
+        public virtual void PresentVehicle(Vehicle vehicle)
+        {
+            MakeSound();
+            
+            Console.WriteLine($"Color: {vehicle.VehicleColor} Type: {vehicle.VehicleTyp}");
+        }
+
+    }
+}
